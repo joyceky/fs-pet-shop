f0b62b0faf7818a63f4dbd5834b645aa9065c822
Kyle Joyce <kyyjoyce@gmail.com>
2016-11-08 15:29:48 -0700

    read working with index and error handling for index

--- pets.js
+++ pets.js
@@ -2,25 +2,39 @@
 
 var fs = require('fs');
 var path = require('path');
-var guestsPath = path.join(__dirname, 'pets.json');
+var petsPath = path.join(__dirname, 'pets.json');
 
 var node = path.basename(process.argv[0]);
 var file = path.basename(process.argv[1]);
 var cmd = process.argv[2];
+var index = process.argv[3];
+
 
 if (cmd === 'read') {
-fs.readFile(guestsPath, 'utf8', function(err, data) {
+fs.readFile(petsPath, 'utf8', function(err, data) {
+  var dataP = JSON.parse(data);
+
   if (err) {
     throw err;
   }
 
-  var pets = JSON.parse(data);
+  else if (index === undefined) {
+    var pets = dataP;
+    console.log(pets);
+    process.exit(1);
+  }
 
-  console.log(pets);
+  else if (index > dataP.length - 1 || index < 0) {
+    console.error(`Usage: ${node} ${file} index`);
+    process.exit(1);
+  }
+
+    console.log(index);
+    var pet = dataP;
+    console.log(pet[index]);
  });
 }
-
-else {
-  console.error(`Usage: ${node} ${file} read`);
-  process.exit(1);
-}
+  else {
+    console.error(`Usage: ${node} ${file} read`);
+    process.exit(1);
+  }

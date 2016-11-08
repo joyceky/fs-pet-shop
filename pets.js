6acef463aab72f4c86452c00977094ef6d0d00e1
Kyle Joyce <kyyjoyce@gmail.com>
2016-11-08 11:44:15 -0700

    working with read command

--- pets.js
+++ pets.js
@@ -4,6 +4,11 @@ var fs = require('fs');
 var path = require('path');
 var guestsPath = path.join(__dirname, 'pets.json');
 
+var node = path.basename(process.argv[0]);
+var file = path.basename(process.argv[1]);
+var cmd = process.argv[2];
+
+if (cmd === 'read') {
 fs.readFile(guestsPath, 'utf8', function(err, data) {
   if (err) {
     throw err;
@@ -12,4 +17,10 @@ fs.readFile(guestsPath, 'utf8', function(err, data) {
   var pets = JSON.parse(data);
 
   console.log(pets);
-});
+ });
+}
+
+else {
+  console.error(`Usage: ${node} ${file} read`);
+  process.exit(1);
+}

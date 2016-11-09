677d40bc70bda91351c7d68ce5c1e975db86e37e
Kyle Joyce <kyyjoyce@gmail.com>
2016-11-09 16:45:00 -0700

    expressServer working

--- expressServer.js
+++ expressServer.js
@@ -1,37 +1,119 @@
-"use strict";
+'use strict';
 
-//Lets require/import the HTTP module
-var http = require('http');
 var fs = require('fs');
-var pets = JSON.parse(fs.readFileSync('./pets.json', 'utf8'));
+var path = require('path');
+var petsPath = path.join(__dirname, 'pets.json');
 
-//Lets define a port we want to listen to
-const PORT=8000;
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 8000;
 
+app.disable('x-powered-by');
 
-const petRegExp = /^\/pets\/(.*)$/;
+app.get('/pets', function(req, res) {
+  fs.readFile(petsPath, 'utf8', function(err, petsJSON) {
+    if (err) {
+      console.error(err.stack);
+      return res.sendStatus(500);
+    }
 
-//We need a function which handles requests and send response
-function handleRequest(request, response){
-    response.end('It Works!! Path Hit: ' + request.url);
-    console.log(pets);
+    var pets = JSON.parse(petsJSON);
 
-    server.on('request', function(request, response) {
-      // the same kind of magic happens here!
-      if(request === "GET" && request.url === "/pets") {
-        console.log(pets);
-      }
-      else {
-        //do something else
-      }
-    });
-}
-
-//Create a server
-var server = http.createServer(handleRequest);
-
-//Lets start our server
-server.listen(PORT, function(){
-    //Callback triggered when server is successfully listening. Hurray!
-    console.log("Server listening on: http://localhost:%s", PORT);
+    res.send(pets);
+  });
 });
+
+app.get('/pets/:id', function(req, res) {
+  fs.readFile(petsPath, 'utf8', function(err, petsJSON) {
+    if (err) {
+      console.error(err.stack);
+      return res.sendStatus(500);
+    }
+
+    var id = Number.parseInt(req.params.id);
+    var pets = JSON.parse(petsJSON);
+
+    if (id < 0 || id >= pets.length || Number.isNaN(id)) {
+      return res.sendStatus(404);
+    }
+
+    res.set('Content-Type', 'application/json');
+    res.send(pets[id]);
+  });
+});
+
+app.use(function(req, res) {
+  res.sendStatus(404);
+});
+
+app.listen(port, function() {
+  console.log('Listening on port', port);
+});
+
+module.exports = app;
+
+
+
+
+
+
+
+
+
+//
+// var server = http.createServer(function(req, res) {
+//   if (req.method === 'GET' && req.url === '/pets') {
+//     fs.readFile(petsPath, 'utf8', function(err, petsJSON) {
+//       if (err) {
+//         console.error(err.stack);
+//         res.statusCode = 500;
+//         res.setHeader('Content-Type', 'text/plain');
+//         return res.end('Internal Server Error');
+//       }
+//
+//       res.setHeader('Content-Type', 'application/json');
+//       res.end(petsJSON);
+//     });
+//   }
+//   else if (req.method === 'GET' && req.url === '/pets/0') {
+//     fs.readFile(petsPath, 'utf8', function(err, petsJSON) {
+//       if (err) {
+//         console.error(err.stack);
+//         res.statusCode = 500;
+//         res.setHeader('Content-Type', 'text/plain');
+//         return res.end('Internal Server Error');
+//       }
+//
+//       var pets = JSON.parse(petsJSON);
+//       var petJSON = JSON.stringify(pets[0]);
+//
+//       res.setHeader('Content-Type', 'application/json');
+//       res.end(petJSON);
+//     });
+//   }
+//   else if (req.method === 'GET' && req.url === '/pets/1') {
+//     fs.readFile(petsPath, 'utf8', function(err, petsJSON) {
+//       if (err) {
+//         console.error(err.stack);
+//         res.statusCode = 500;
+//         res.setHeader('Content-Type', 'text/plain');
+//         return res.end('Internal Server Error');
+//       }
+//
+//       var pets = JSON.parse(petsJSON);
+//       var petJSON = JSON.stringify(pets[1]);
+//
+//       res.setHeader('Content-Type', 'application/json');
+//       res.end(petJSON);
+//     });
+//   }
+//   else {
+//     res.statusCode = 404;
+//     res.setHeader('Content-Type', 'text/plain');
+//     res.end('Not found');
+//   }
+// });
+//
+// server.listen(port, function() {
+//   console.log('Listening on port', port);
+// });
